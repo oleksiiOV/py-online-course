ed8b9fe236571e891553d56f3ad633b51ac10aa3
oleksiiOV <116111654+oleksiiOV@users.noreply.github.com>
2025-02-13 00:14:08 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 class OnlineCourse:
-    def __init__(self, name: str, description: str, weeks: int):
+    def __init__(self, name: str, description: str, weeks: int) -> None:
         self.name = name
         self.description = description
         self.weeks = weeks
-    
+
     @staticmethod
     def days_to_weeks(days: int) -> int:
         return (days + 6) // 7  # Rounds up to the next whole week
-    
+
     @classmethod
-    def from_dict(cls, course_dict: dict):
+    def from_dict(cls, course_dict: dict) -> None:
         weeks = cls.days_to_weeks(course_dict["days"])
         return cls(course_dict["name"], course_dict["description"], weeks)
-    
+
     def __repr__(self) -> str:
-        return f"OnlineCourse(name='{self.name}', description='{self.description}', weeks={self.weeks})"
+        return (f"OnlineCourse(name='{self.name}'"
+                f", description='{self.description}'"
+                f", weeks={self.weeks})")
